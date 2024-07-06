b140df0ad9bb5c6261ba1cab515d95a384850bfd
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2024-07-06 19:11:17 +0530

    Update Todoitems.jsx

--- TodoApp/src/Components/Todoitems.jsx
+++ TodoApp/src/Components/Todoitems.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import './CSS/Todoitems.css';
-import tick from './Assets/tick.png';
-import notTick from './Assets/not_tick.png';
-import cross from './Assets/cross.png';
+import React from "react";
+import "./CSS/Todoitems.css";
+import tick from "./Assets/tick.png";
+import notTick from "./Assets/not_tick.png";
+import cross from "./Assets/cross.png";
 
 const Todoitems = ({ text, isCompleted, onComplete, onDelete }) => {
   return (
     <div className="todoitems">
       <div className="todoitems-container">
-        <img 
-          src={isCompleted ? tick : notTick} 
-          alt={isCompleted ? "Completed" : "Not Completed"} 
-          onClick={onComplete}
-        />
-        <div className={`todoitems-text ${isCompleted ? 'completed' : ''}`}>{text}</div>
+        <img src={isCompleted ? tick : notTick} onClick={onComplete} />
+        <div>
+          {text}
+        </div>
       </div>
       <img src={cross} alt="Delete" onClick={onDelete} />
     </div>
